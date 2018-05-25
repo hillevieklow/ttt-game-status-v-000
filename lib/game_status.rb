f8f17ad6bbb20020140240bd536f3c54d9c181bd
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-25 15:04:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,3 +16,21 @@ WIN_COMBINATIONS = [
   [2,4,6]
   # an array for each win combination
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    else
+      false
+end
